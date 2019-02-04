e121452c8808966e0d233806668d95ec9ca4b114
Max <Ponomarev1802@mail.ru>
2019-02-04 18:18:07 +0300

    Добавлены поля в модели User и Wish

--- WishBoard/user/models.py
+++ WishBoard/user/models.py
@@ -6,10 +6,12 @@ import json
 class User(BaseModel):
     name = CharField(max_length=30)
     surename = CharField(max_length=30, null=True)
+    nickname = CharField(max_length=30)
     email = CharField(max_length=50, unique=True)
     password = CharField()
     photo = CharField(null=True)
     birth_date = DateField(null=True)
+    session_key = CharField(max_length=100)
 
     def serialize(self):
         return {"name": self.name,
@@ -17,3 +19,9 @@ class User(BaseModel):
                 "photo": self.photo,
                 "birth_date": str(self.birth_date),
                 }
+
+class Followers(BaseModel):
+    toward = ForeignKeyField(User, on_delete="CASCADE")
+    whom = ForeignKeyField(User, on_delete="CASCADE")
+
+
--- WishBoard/wish/models.py
+++ WishBoard/wish/models.py
@@ -1,17 +1,25 @@
-from peewee import CharField, DateField, IntegerField, ForeignKeyField, TextField
+from peewee import CharField, DateField, IntegerField, ForeignKeyField, TextField, DateTimeField, BooleanField
 from helpers.models import BaseModel
+import datetime
 
 from user.models import User
 
 
 class Wish(BaseModel):
-    user = ForeignKeyField(User, on_delete='CASCADE', related_name='wishes')
+    user = ForeignKeyField(User, on_delete='cascade', related_name='wishes')
     title = CharField(max_length=50)
     description = TextField()
     image = CharField()
-    href = CharField(max_length=250)
+    link = CharField(max_length=250)
     cost = IntegerField()
     balance = IntegerField()
+    donator = ForeignKeyField(User, on_delete='SET NULL', related_name='wishes')
+    creationdate = DateField(default=datetime.datetime.today())
+    expiry = DateField(default=None)
+    category = CharField(max_length=50)
+    status = CharField(max_length=3)
+    isHidden = BooleanField()
+    from_user = ForeignKeyField(User, on_delete='CASCADE', related_name='wishes')
 
     def serialize(self):
         return {"id": self.id,
@@ -21,3 +29,18 @@ class Wish(BaseModel):
                 "href": self.href,
                 "cost": self.cost
                 }
+
+
+class Comments(BaseModel):
+    user = ForeignKeyField(User, on_delete='cascade', related_name='comments')
+    wish = ForeignKeyField(Wish, on_delete='cascade', related_name='comments')
+    time = DateTimeField(default=datetime.datetime.now)
+    text = TextField()
+
+
+Comments.response = ForeignKeyField(Comments, on_delete='cascade', related_name='response')
+
+
+class ExcludeHidden:
+    wish = ForeignKeyField(Wish, on_delete='cascade')
+    user = ForeignKeyField(User, on_delete='cascade')
