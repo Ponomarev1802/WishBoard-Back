864d1242fef276ad19847245f5581614868d854c
P.Maks <ponomarev1802@mail.ru>
2019-02-20 22:19:15 +0300

    Удаление тестовых модулей

--- WishBoard/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from accounts import views
-
-routes = (
-    dict(method='*', path='/register', handler=views.Register, name='register'),
-    dict(method='*', path='/login', handler=views.LogIn, name='login'),
-    dict(method='GET', path='/logout', handler=views.LogOut, name='logout'),
-)
--- WishBoard/index/urls.py
+++
@@ -1,5 +0,0 @@
-from index.views import Index
-
-routes = (
-    dict(method='GET', path='/', handler=Index, name='index'),
-)
